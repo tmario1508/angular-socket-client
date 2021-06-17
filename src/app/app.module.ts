dbc91cd780953b04e80a0d977cae6c1c8dd72961
Mario Torres <tmario1508@gmail.com>
2021-06-17 09:32:46 -0500

    Update test app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,19 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
-import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment';
 import { UsersComponent } from './pages/users/users.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const FIREBASE_SETTINGS = {
+  apiKey: "AIzaSyDrzTcoxAf5WhexxzSVnN0QyNA1oIvGGf0",
+  authDomain: "oauth2-f626c.firebaseapp.com",
+  projectId: "oauth2-f626c",
+  storageBucket: "oauth2-f626c.appspot.com",
+  messagingSenderId: "305293313491",
+  appId: "1:305293313491:web:40b816aef00c945fcd5b26",
+  measurementId: "G-72HQR6KHFC"
+}
 
 @NgModule({
   declarations: [
@@ -32,7 +41,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     BrowserModule,
     AppRoutingModule,
     SocketIoModule.forRoot(config),
-    AngularFireModule.initializeApp(environment.FIREBASE_SETTINGS),
+    AngularFireModule.initializeApp(FIREBASE_SETTINGS),
     AngularFireAuthModule
   ],
   providers: [],
